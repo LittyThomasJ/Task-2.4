a51921e3326b2cb2c00890eb6d8c07d130dc4355
SijithThomas <79042404+SijithThomas@users.noreply.github.com>
2021-03-28 09:19:06 +0530

    Update checkemail.php

--- checkemail.php
+++ checkemail.php
@@ -1,12 +1,14 @@
 <?php
-include_once('connection.php');
-session_start();
-
-if (isset($_POST['email_check'])) {
+// including connection page
+  include_once('connection.php');
+  // Session is started
+  session_start();
+  if (isset($_POST['email_check'])) {
   	$email = $_POST['Email'];
-
+    // select query
   	$sql = "SELECT email FROM tb_reg WHERE email='$email'";
     $obj=new DbFunctions();
+    // check email exists or not
     $select=$obj->Check_email_exist($sql);
   	if (mysqli_num_rows($select) > 0) {
   	  echo "taken";
